bbba7ed394c4b6219a264b213e0019abfed00447
hunamuna123 <95960372+hunamuna123@users.noreply.github.com>
2025-04-17 08:45:17 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <HeaderNavbar/>
+</template>
---
+++ plugins/preline.client.ts
@@ -0,0 +1,12 @@
+import { useRouter } from "vue-router";
+
+// Preline UI
+import("../node_modules/preline/dist");
+
+export default defineNuxtPlugin(() => {
+  const router = useRouter();
+
+  router.afterEach(async () => {
+    setTimeout(() => window.HSStaticMethods.autoInit());
+  });
+});
