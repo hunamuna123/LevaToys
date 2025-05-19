4df23c2a5668a4dfbc017a6379dbdaa121cffdb1
hunamuna123 <95960372+hunamuna123@users.noreply.github.com>
2025-05-20 02:39:16 +0300

    navbar slider pwapp basket fix and more

--- app.vue
+++ app.vue
@@ -1,4 +1,10 @@
 <template>
- <NuxtLoadingIndicator color="#10b981" height="4" duration="1000" throttle="500" />
+  <NuxtLoadingIndicator color="#10b981" :height="4" :duration="500" :throttle="300" />
   <NuxtPage/>
 </template>
+
+<script setup>
+useHead({
+  viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+})
+</script>
